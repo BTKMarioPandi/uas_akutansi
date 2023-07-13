c66600b1820f89000202df398d2afa97a57c9b13
Mario Pandi <50074080+BTKMarioPandi@users.noreply.github.com>
2023-07-13 23:11:46 +0700

    uas fix

--- resources/views/mario_layout/mario_template.blade.php
+++ resources/views/mario_layout/mario_template.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>UUAS Akuntansi</title>
+  <title>Uas Akuntansi</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -169,7 +169,7 @@
           <img src="{{ asset('admin/dist/img/profil.png') }}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">ABDUL BARI'</a>
+          <a href="#" class="d-block">Mario Pandi'</a>
         </div>
       </div>
 
